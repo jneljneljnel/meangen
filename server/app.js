ecaf80e5463c2dd945f2c00d75c56d31caa6cffc
jneljneljnel <whoisjeremynelson@gmail.com>
2015-09-19 17:12:04 -0500

    heroku needs db

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to database
-mongoose.connect('mongodb://Jeremy:Treepick1@ds051543.mongolab.com:51543/heroku_tmgkzb5v');
+mongoose.connect(config.mongo.uri, config.mongo.options);
 mongoose.connection.on('error', function(err) {
 	console.error('MongoDB connection error: ' + err);
 	process.exit(-1);
